fa00ba6f120c0f2fe719e8e0ee2c830890cc7060
zcchann <z2023234@gmail.com>
2019-12-26 11:45:08 +0800

    拆分文件

--- trace_anime.py
+++ trace_anime.py
@@ -6,13 +6,13 @@ import logging
 import base64
 import hashlib
 import hmac
+from image import sousuo
 
 #设置日志
 logging.basicConfig(filename="./trace.log",format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 #读取json文件内的参数
-set = open("config.json",encoding='utf-8')
-seting = json.load(set)
+seting = json.load(open("config.json",encoding='utf-8'))
 line_bot = seting["line_bot_Channel_access_token"]
 saucenao_key = seting["saucenao_api_key"]
 line_bot_hannel = seting["line_bot_Channel_secret"]
@@ -57,49 +57,50 @@ def callback():
                         fd.write(chunk)
 
                 images_url = domain + image_id + ".jpg"
-                search_image_url = saucenao_url + saucenao_key + "&url="
-                response = requests.get(url=search_image_url + images_url)  # 获取trace.moe的返回信息
-                response.encoding = 'utf-8'  # 把trace.moe的返回信息转码成utf-8
-                result = response.json()  # 转换成json格式
-                try:
-                    status = result["header"]["status"]
-                except:
-                    status = 1
+                search_image_url = saucenao_url + saucenao_key + "&url=" + images_url
 
-                if status < 1:
-                    vaule = "今日机器人搜索次数已达上限 请于24小时后再进行搜索"
-                else:
-                    similarity = result['results'][0]['header']['similarity']  # 相似度
-                    try:
-                        jp_name = result['results'][0]['data']['jp_name']
-                    except KeyError:
-                        jp_name = ""
-                    try:
-                        ext_urls = result['results'][0]['data']['ext_urls'][0]
-                    except KeyError:
-                        ext_urls = ""
-                    try:
-                        pixiv_id = int(result['results'][0]['data']['pixiv_id'])
-                    except KeyError:
-                        pixiv_id = ""
-                    try:
-                        member_name = result['results'][0]['data']['member_name']
-                    except KeyError:
-                        member_name = ""
-                    try:
-                        title = result['results'][0]['data']['title']
-                    except KeyError:
-                        title = ""
-                    vaule = "相似度 " + str(similarity) + '%' + '\n' + "作者名称 " + str(member_name) + '\n' + "图片名称 " + str(
-                        title) + '' + jp_name + '\n' + "P站id " + str(pixiv_id) + '\n' + "图片链接 " + '\n' + ext_urls
-                    huifu = {
-                        "replyToken": reply,
-                        "messages": [{
-                            "type": "text",
-                            "text": vaule + '\n' + "本日机器人搜索剩余次数" + number[0]
-                    }]
-                    }
-                    requests.post(url=reply_url, data=json.dumps(huifu), headers=header)
+                # response = requests.get(url=search_image_url)  # 获取trace.moe的返回信息
+                # response.encoding = 'utf-8'  # 把trace.moe的返回信息转码成utf-8
+                # result = response.json()  # 转换成json格式
+                # try:
+                #     status = result["header"]["status"]
+                # except:
+                #     status = 1
+                #
+                # if status < 1:
+                #     vaule = "今日机器人搜索次数已达上限 请于24小时后再进行搜索"
+                # else:
+                #     similarity = result['results'][0]['header']['similarity']  # 相似度
+                #     try:
+                #         jp_name = result['results'][0]['data']['jp_name']
+                #     except KeyError:
+                #         jp_name = ""
+                #     try:
+                #         ext_urls = result['results'][0]['data']['ext_urls'][0]
+                #     except KeyError:
+                #         ext_urls = ""
+                #     try:
+                #         pixiv_id = int(result['results'][0]['data']['pixiv_id'])
+                #     except KeyError:
+                #         pixiv_id = ""
+                #     try:
+                #         member_name = result['results'][0]['data']['member_name']
+                #     except KeyError:
+                #         member_name = ""
+                #     try:
+                #         title = result['results'][0]['data']['title']
+                #     except KeyError:
+                #         title = ""
+                #     vaule = "相似度 " + str(similarity) + '%' + '\n' + "作者名称 " + str(member_name) + '\n' + "图片名称 " + str(
+                #         title) + '' + jp_name + '\n' + "P站id " + str(pixiv_id) + '\n' + "图片链接 " + '\n' + ext_urls
+                #     huifu = {
+                #         "replyToken": reply,
+                #         "messages": [{
+                #             "type": "text",
+                #             "text": vaule + '\n' + "本日机器人搜索剩余次数" + number[0]
+                #     }]
+                #     }
+                requests.post(url=reply_url, data=json.dumps(sousuo(search_image_url)), headers=header)
 
     elif number[0] <= 0:
         vaule = "今日机器人搜索次数已达上限 请于24小时后再进行搜索"
